2aaee73d4007fd0e2e8a884de469398a6b8d844d
Montacargas <95055582+Montacargas@users.noreply.github.com>
2021-11-25 16:35:33 -0600

    Add files via upload

---
+++ phishing/infografia.php
@@ -0,0 +1,295 @@
+	
+	<link rel="stylesheet" href="css3/css/style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="css3/css/icomoon.css">
+	
+	
+	<title>PSHISHING</title>
+
+
+<style>
+
+	h3{
+		color: lightgray;
+	}
+
+		body{
+			background-color: #007be0;
+		}
+	
+	.padd{
+		background-color: black;
+		margin:auto;
+		padding: 10px 0px 50px;
+	}
+
+	<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin:auto;
+				width:400px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#000;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 5px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background-color:#434343;
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+
+			.ventana{
+				background: white;
+				width: 90%;
+				color:black;
+				font-family:Arial, Helvetica, sans-serif;
+				font-size: 18px;
+				text-align: left;
+				padding: 30px;
+				min-height: 250px;
+				border-radius: 22px;
+				position: absolute;
+			}
+
+
+			.inform{
+				background-color: black;
+				height: 200px;
+				display: flex;
+				justify-content: center;
+				align-content: center;
+			}
+
+			small{
+				color: white;
+			}
+
+			.colores{
+				color: white;
+			}
+
+			.interior{
+				
+				text-align: center;
+			}
+
+			.interior2{
+				padding-right: 45px;
+			}
+
+		</style>
+
+
+
+
+	<!-- cabezera -->
+
+	<div class="padd">
+
+		<!-- lista de items -->
+
+		<div id="header">
+			<ul class="nav">
+				
+				<li><a href="#">Pasos contra el pshishing</a>
+					<ul>
+						<li><a href="#">Aprende a identificar</a>
+							<ul>
+								<div class="ventana">
+									<p>Existen algunos aspectos que inequívocamente, identifican este tipo de ataques a través de correo electrónico como son la ortografía y que siempre pedirá datos personales que ya deberían tener  </p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Verifica la fuente</a>
+							<ul>
+								<div class="ventana">
+									<p> Tu banco nunca te pedirá que le envíes tus claves o datos personales por correo. Nunca respondas a este tipo de preguntas y si tienes una mínima duda, llama directamente a tu banco para aclararlo. </p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">No pulses enlaces</a>
+							<ul>
+								<div class="ventana">
+									<p>No hagas clic en los hipervínculos o enlaces que te adjunten en el correo, ya que de forma oculta te podrían dirigir a una web fraudulenta.</p>
+									<p>Teclea directamente la dirección web en tu navegador o utiliza marcadores/favoritos si quieres ir más rápido.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Refuerza tu seguridad</a>
+							<ul>
+								<div class="ventana">
+									<p>El sentido común y la prudencia es tan indispensable como mantener tu equipo protegido con un buen antivirus que bloquee este tipo de ataques. Además, siempre debes tener actualizado tu sistema operativo y navegadores web.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Webs seguras</a>
+							<ul>
+								<div class="ventana">
+									<p> Las webs seguras han de empezar por ‘https://’ y debe aparecer en tu navegador el icono de un pequeño candado cerrado.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Se prudente</a>
+							<ul>
+								<div class="ventana">
+									<p>La mejor forma de acertar siempre es rechazar de forma sistemática cualquier correo electrónico o comunicado que incida en que facilites datos confidenciales.</p>
+									<p>Elimina este tipo de correos y llama a tu entidad bancaria para aclarar cualquier duda.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">No descargues</a>
+							<ul>
+								<div class="ventana">
+									<p>Esos archivos pueden tener virus o software malicioso que podrían permitir a un atacante acceder a su computadora y, por lo tanto, a toda la información que almacene o introduzca en ella. </p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Mensajes sospechosos</a>
+							<ul>
+								<div class="ventana">
+									<p>Si le envían un SMS de bienvenida a un servicio que no ha contratado, bórrelo y olvídese de él. </p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Informate periodicamente</a>
+							<ul>
+								<div class="ventana">
+									<p>Mantente siempre informado sobre los virus informáticos, porque conociendo su función nos será más fácil saber protegernos.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Actualizaciones</a>
+							<ul>
+								<div class="ventana">
+									<p>Asegurarse de que el SO, el navegador y otro software crítico estén actualizados.</p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Contraseñas fuertes</a>
+							<ul>
+								<div class="ventana">
+									<p>La contraseña debe tener una longitud de, al menos, 8 caracteres. </p>
+									<p>La contraseña debe contener letras mayúsculas y minúsculas.</p>
+									<p>La contraseña debe contener un número. </p>
+									<p>La contraseña debe contener un carácter especial. </p>
+									<p>Elija una contraseña que pueda recordar. </p>
+									<p>Elija una contraseña que otra persona no pueda asociar con usted. </p>
+									<p>Elija contraseñas diferentes y nunca use la misma contraseña para servicios diferentes. </p>
+									<p>Está bien usar contraseñas complejas siempre que esto no las haga difíciles de recordar. </p>
+								</div>
+							</ul>
+						</li>
+						<li><a href="#">Informacion personal</a>
+							<ul>
+								<div class="ventana">
+									<p>No ponga información personal en computadoras publicas como puede ser una biblioteca, un ciber o cualquier equipo que no sea de tu propiedad y que no sea confiable. </p>
+								</div>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="#">Estafas comúnes</a>
+					<ul>
+						<li>
+							<a href="https://www.consumer.ftc.gov/blog/2018/12/fake-calls-about-your-ssn" target="_blanck">Llamadas falsas sobre su seguro social</a>
+						</li>
+						<li>
+							<a href="https://www.consumer.ftc.gov/articles/phone-scams" target="_blanck">Estafas telefonicas</a>
+						</li>
+						<li>
+							<a href="https://www.consumer.ftc.gov/articles/how-recognize-and-avoid-phishing-scams" target="_blanck">Estafas de phishing</a>
+						</li>
+						<li>
+							<a href="https://www.consumer.ftc.gov/blog/2020/06/scammer-getting-unemployment-benefits-your-name" target="_blanck">Estafas de beneficios de desempleo</a>
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Reportar</a>
+					<ul>
+						<li><a href="https://reportfraud.ftc.gov/#/" target="_blanck">Informar estafas a la FTC</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+
+		<!-- fin de lista de items -->
+
+	</div>
+	<!-- Fin de la cabezera -->
+
+
+<!-- centro de informacion -->
+<div align="center">
+		<img src="imagen/image.png" width="683px" height="569px">
+</div>
+
+<div align="center">
+		<img src="imagen/captura00.png" width="683px" height="640px">	
+</div>
+<!-- Fin del centro de informacion -->
+
+
+<!-- Padding de informacion -->
+
+<div class="inform"> 
+									<!-- Contactos e informacion -->
+					<div class="interior">
+                        <h3>Contactos</h3>
+                        <ul class="gtco-quick-contact">
+                            <li><a class="colores"><i class="icon-phone"></i> Tel (444)8-24-02-90 ext.107 </a></li>
+                        </ul>
+                        	 <!-- contenedor para aliniar -->
+                        <div class="interior2">
+                        	<ul>
+                        		<li><a class="colores"><i class="icon-phone"></i> Celular. 4448459844 </a></li>
+                        	</ul>
+                        </div>
+                        
+                        	<!-- Derechos y autores -->
+                        <p><small>&copy; 2021 Montacargas AC. All Rights Reserved.</small> 
+                        <small>Departamento de Sistemas. <a class="colores"> Jessica Tello Valtierra</a> y <a class="colores">Jose Angel Rangel Rangel</a></small></p>
+                    </div>
+
+                    				<!-- Fin de contactos e informacion -->
+
+</div>
+
+<!-- fin del padding -->
